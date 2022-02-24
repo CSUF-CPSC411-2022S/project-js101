460c0999bb1fe84d61905c262622fd94cb8173ce
Jeff Martz <jeffmartz11@gmail.com>
2022-02-23 17:08:50 -0800

    Initial Xcode Project

---
+++ BirdSpot/BirdSpot/BirdSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdSpotApp.swift
+//  BirdSpot
+//
+//  Created by csuftitan on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BirdSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdSpot/BirdSpot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BirdSpot
+//
+//  Created by csuftitan on 2/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Text("Ya like birds?")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
