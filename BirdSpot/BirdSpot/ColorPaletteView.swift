5dcf019b8a9e64be518e513b9486fd7b2162ce7b
csuftitan <csuftitan@MacBook-Air-6.lan>
2022-03-09 23:55:04 -0800

    feature: added all of the colors to match UI markup - TODO: add naviagtional links to each color

--- BirdSpot/BirdSpot/ColorPaletteView.swift
+++ BirdSpot/BirdSpot/ColorPaletteView.swift
@@ -15,7 +15,7 @@ struct ColorPaletteView: View {
     var body: some View {
         GeometryReader { geometry in
             NavigationView {
-                // this vstack acts as a container for the child hstacks
+                // This Vstack acts as a container for the entire page
                 VStack {
                     // creating horizontal stacks so welcome and select messages can be left justified.
                     HStack {
@@ -32,7 +32,9 @@ struct ColorPaletteView: View {
                             .padding()
                         Spacer()
                     }
+                    
                     // TODO: run tests to ensure working textfield
+                    // Hstack creates textfield with search icon.
                     HStack {
                         Image("search-icon")
                             .resizable().scaledToFit()
@@ -43,6 +45,38 @@ struct ColorPaletteView: View {
                         .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.black, lineWidth: 2))
                         .frame(width: geometry.size.width * 0.90, height: geometry.size.height * 0.05)
                     
+                    // Vstack for the colors! TODO: make the individual circles naviagtion links
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.red)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.gray)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.yellow)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                        }.padding()
+                        
+                        HStack {
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.green)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.blue)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.purple)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                        }.padding()
+                        
+                        HStack {
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.brown)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.white)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                            Circle().strokeBorder(Color.black, lineWidth: 3).background(Circle().foregroundColor(Color.black)).frame(width: geometry.size.width * 0.2, height: geometry.size.width * 0.2)
+                            Spacer()
+                        }.padding()
+                    }
                 
                 }
             }
