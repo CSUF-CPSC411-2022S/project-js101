1cc7a5d52c1652d580a7ac431d983c2493a108c5
csuftitan <csuftitan@MacBook-Air-6.lan>
2022-03-10 00:34:47 -0800

    feature: no valid unit test at the moment

---
+++ BirdSpot/BirdSpotTests/ColorPaletteViewTest.swift
@@ -0,0 +1,13 @@
+//
+//  ColorPaletterViewTest.swift
+//  BirdSpotTests
+//
+//  Created by csuftitan on 3/10/22.
+//
+
+import XCTest
+@testable import BirdSpot
+
+class ColorPaletteTests: XCTestCase {
+    // No valid unit tests for current view.
+}
